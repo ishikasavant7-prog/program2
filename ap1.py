c0559254061629179674f5273635451146dd4862
ishikasavant7-prog <ishikasavant7@gmail.com>
2025-10-28 13:49:13 +0530

    commit

---
+++ ap1.py
@@ -0,0 +1,6 @@
+#print()function in python
+#print()using coucateenation using + symbol
+a=10
+b=20
+sum=a+b
+print('sum=+'+str(sum))
---
+++ ap2.py
@@ -0,0 +1,6 @@
+#1.print() function in python
+#2. print() using concatanation method using + symbol
+a=20
+b=30
+sub=a-b
+print('sub of 2 numbers='+str(sub))
